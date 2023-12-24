caa17712d5b4f58b60ff0f87344e30bf5da264da
somervda <davidsomerville@comcast.net>
2023-12-24 14:56:58 -0500

    first commit

---
+++ rpimidi_midi_test.py
@@ -0,0 +1,23 @@
+import time
+import rtmidi
+
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
+print("available_ports", available_ports)
+
+if available_ports:
+    midiout.open_port(0)
+else:
+    midiout.open_virtual_port("My virtual output")
+
+with midiout:
+    note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
+    note_off = [0x80, 60, 0]
+    midiout.send_message(note_on)
+    print("Note On")
+    time.sleep(0.5)
+    midiout.send_message(note_off)
+    print("Note Off")
+    time.sleep(0.1)
+
+del midiout
